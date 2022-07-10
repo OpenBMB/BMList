568689fca39910285e954f3d85fba5e2c4403194
zh-zheng <qishuangyiren@outlook.com>
2022-07-10 23:33:41 +0800

    update

--- README.md
+++ README.md
@@ -1,60 +1,8 @@
 # BMList
 
-<!-- ## Contents
-
-* [Introduction](#introduction)
-* [Big Models](#big-models)
-    * [2022-06](#2022-06)
-    * [2022-05](#2022-05)
-    * [2022-04](#2022-04)
-    * [2022-03](#2022-03)
-    * [2022-01](#2022-01)
-    * [2021-12](#2021-12)
-    * [2021-11](#2022-11)
-    * [2021-09](#2021-09)
-    * [2021-08](#2021-08)
-    * [2021-07](#2021-07)
-    * [2021-06](#2021-06)
-    * [2021-05](#2021-05)
-    * [2021-04](#2021-04)
-    * [2021-03](#2021-03)
-    * [2021-01](#2021-01)
-    * [2020-12](#2020-12)
-    * [2020-05](#2020-05)
-    * [2020-04](#2020-04)
-    * [2020-03](#2020-03)
-    * [2020-01](#2020-01)
-    * [2019-10](#2019-10)
-    * [2019-02](#2019-02)
-* [Contributors](#contributors) -->
-
 ## Introduction
 
-## Big Models
+## Big Models Gallery
 
-### 2022-06
-
-1. ![](https://img.shields.io/badge/-Code-gray) ![](https://img.shields.io/badge/-Model-gray) ![](https://img.shields.io/badge/-Chinese-blue) **OBERT** by OPPO. [[News]](https://blog.51cto.com/u_15273780/5440502) 
-    * ~1B 
-
-### 2022-05
-
-1. ![](https://img.shields.io/badge/-Code-gray) ![](https://img.shields.io/badge/-API-green) ![](https://img.shields.io/badge/-Chinese-blue) **ERNIE 3.0 Zeus** by Baidu. [[News]](https://baijiahao.baidu.com/s?id=1733603775259242015&wfr=spider&for=pc) 
-    * ~100B [[API]](https://wenxin.baidu.com/younger/apiDetail?id=20006)
-
-2. ![](https://img.shields.io/badge/-Code-green) ![](https://img.shields.io/badge/-Model-green) ![](https://img.shields.io/badge/-English-blue) **OPT** by Meta AI. [[Paper]]((https://arxiv.org/abs/2205.01068)) [[Code]](https://github.com/facebookresearch/metaseq) 
-    * 1.3B, 2.7B, 6.7B, 13B, 30B, 66B [[Model]](https://github.com/facebookresearch/metaseq/tree/main/projects/OPT#pretrained-model-weights)
-    * 175B [[Model request]](https://github.com/facebookresearch/metaseq/tree/main/projects/OPT#pretrained-model-weights)
-
-3. ![](https://img.shields.io/badge/-Code-green) ![](https://img.shields.io/badge/-Model-green) ![](https://img.shields.io/badge/-English-blue) **UL2** by Google. [[Paper]](https://arxiv.org/abs/2205.05131) [[Code]](https://github.com/google-research/t5x)
-    * 20B [[Model]](https://huggingface.co/google/ul2/tree/main)
-
-4. ![](https://img.shields.io/badge/-Code-gray) ![](https://img.shields.io/badge/-Model-gray) ![](https://img.shields.io/badge/-Chinese-blue) **HuanYuan (混元)** by Tencent. [[News]](http://ex.chinadaily.com.cn/exchange/partners/82/rss/channel/cn/columns/snl9a7/stories/WS628df605a3101c3ee7ad730e.html) 
-    * ~10B
-
-### 2022-04
-
-1. ![](https://img.shields.io/badge/-Code-green) ![](https://img.shields.io/badge/-Model-green) ![](https://img.shields.io/badge/-English-blue) **GPT-NeoX** by EleutherAI. [[Paper]](https://arxiv.org/abs/2204.06745) [[Code]](https://github.com/EleutherAI/gpt-neox) 
-    * 20B [[Model]](https://github.com/EleutherAI/gpt-neox#download-links)
 
 ## Contributors
---
+++ main.py
@@ -0,0 +1,106 @@
+import yaml
+import json
+import os
+import matplotlib.pyplot as plt
+import seaborn as sns
+from collections import defaultdict
+from datetime import datetime
+import matplotlib.dates as mdates
+import math
+
+sns.set_style('darkgrid')
+plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
+plt.rcParams["axes.unicode_minus"] = False
+plt.rcParams['date.converter'] = 'concise'
+
+def get_model_list(main_path):
+    """
+    Get a model list from all yaml files in `main_path`.
+    """
+    files = os.listdir(main_path)
+    model_list = list()
+    
+    for file in files:
+        f = open(os.path.join(main_path, file), 'r')
+        data = yaml.load(f.read(), Loader=yaml.FullLoader)
+        model_list.append(data)
+        f.close()
+    
+    model_list = sorted(model_list, key=lambda x: (x['release_date'], x['name']))
+    
+    return model_list
+
+
+def write_to_jsonl(model_list, output_file):
+    """
+    Write the model_list to a jsonl file.
+    """
+    f = open(output_file, 'w')
+    for model in model_list:
+        f.write(json.dumps(model, ensure_ascii=False) + '\n')
+    f.close()
+
+def get_lang(language):
+    """
+    Get language shown in the diagram
+    """
+    
+    if 'English' in language:
+        lang = 'English'
+    elif 'Chinese' in language:
+        lang = 'Chinese'
+    elif 'Multilingual' in language:
+        lang = 'Multilingual'
+    else:
+        lang = 'Others'
+
+    return lang
+
+def plot_scatter(model_list):
+    """
+    Draw scatter diagram.
+    """
+
+    fig = plt.figure(dpi=300, figsize=(12, 6))
+    parameters = defaultdict(list)
+    dates = defaultdict(list)
+    names = defaultdict(list)
+    color_map = {"English": "green", "Chinese": "orange", "Multilingual": "blue", "Others": "yellow"}
+
+    for model in model_list:
+        # we plot two points if a model have both dense and MoE variants
+        for variant in ['dense', 'MoE']:
+            if 'parameters_{}'.format(variant) in model:
+                lang = get_lang(model['language'])
+                if model['release_date'] < first_date:
+                    first_date = model['release_date']
+                dates[lang].append(model['release_date'])
+
+                param_list = sorted(list(map(lambda x: float(x.split('~')[-1].split('B')[0]), model['parameters_{}'.format(variant)])))
+                parameters[lang].append(param_list[-1])
+                names[lang].append(model['name'] + ('(MoE)' if variant == 'MoE' else '') if parameters[lang][-1] >= 100 else '')
+                
+    for lang in  dates.keys():
+        x = list(map(lambda x: datetime.strptime(x, "%Y/%m/%d"), dates[lang]))
+        plt.scatter(x, parameters[lang], s=list(map(lambda x: x + 10, parameters[lang])), c=color_map[lang], marker='o', alpha=0.7, label=lang)
+    
+        for i, label in enumerate(names[lang]):
+            plt.text(x[i], parameters[lang][i], label, rotation=30, fontsize=7)
+    
+    legend = plt.legend(loc='upper right')
+    for handle in legend.legendHandles:
+        handle.set_sizes([15.0])
+    plt.ylim(-100, 2000)
+    plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator(minticks=12, maxticks=20))
+    plt.xlabel('Date')
+    plt.ylabel('Parameters(B)')
+    plt.savefig('scatter_plot.png', dpi=fig.dpi)
+
+if __name__ == "__main__":
+    main_path = './big_models'
+    model_list = get_model_list(main_path)
+    
+    output_file = 'all_models.jsonl'
+    write_to_jsonl(model_list, output_file)
+
+    plot_scatter(model_list)
