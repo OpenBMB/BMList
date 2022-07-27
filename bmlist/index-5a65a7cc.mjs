f4ce7969299e3fc47a3055062492826aad6b8578
lovangry <272613008@qq.com>
2022-07-27 17:30:04 +0800

    feat: 更新路径

--- bmlist/index-5a65a7cc.mjs
+++ bmlist/index-5a65a7cc.mjs
@@ -1,2 +1,2 @@
-/*! build package in 2022/7/27 15:51:45 ! */
+/*! build package in 2022/7/27 17:18:58 ! */
 import{a as e,b as n}from"./entry-feb83836.mjs";const _=e({__name:"index",setup(t){return n("/trend"),(a,r)=>null}});export{_ as default};
