760a6784bdcabc3b7933779ba938c5b53b833ef1
zh-zheng <qishuangyiren@outlook.com>
2022-07-27 16:06:34 +0800

    init commit for github pages

---
+++ bmlist/index-5a65a7cc.mjs
@@ -0,0 +1,2 @@
+/*! build package in 2022/7/27 15:51:45 ! */
+import{a as e,b as n}from"./entry-feb83836.mjs";const _=e({__name:"index",setup(t){return n("/trend"),(a,r)=>null}});export{_ as default};
