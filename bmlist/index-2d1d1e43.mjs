efddc140c66aaffc1f619ee5dd010cd8371ced07
lovangry <272613008@qq.com>
2022-07-28 18:18:08 +0800

    feat: 适配移动端

--- bmlist/index-2d1d1e43.mjs
+++
@@ -1,2 +0,0 @@
-/*! build package in 2022/7/28 16:08:56 ! */
-import{a as e,b as n}from"./entry-19ffd059.mjs";const _=e({__name:"index",setup(t){return n("/trend"),(a,r)=>null}});export{_ as default};
---
+++ bmlist/useData-2ac80d2a.mjs
@@ -0,0 +1,2 @@
+/*! build package in 2022/7/28 18:15:51 ! */
+var e=()=>fetch("https://raw.githubusercontent.com/OpenBMB/BMList/main/all_models.json").then(t=>t.json());export{e as u};
