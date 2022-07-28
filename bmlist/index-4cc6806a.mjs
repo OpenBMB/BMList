b4b6eacbf0834d8fd071a87ebfd4a05165134d90
lovangry <272613008@qq.com>
2022-07-28 10:20:19 +0800

    fix: 修改更新日期

---
+++ bmlist/index-4cc6806a.mjs
@@ -0,0 +1,2 @@
+/*! build package in 2022/7/28 10:16:11 ! */
+import{a as e,b as n}from"./entry-b5e84022.mjs";const _=e({__name:"index",setup(t){return n("/trend"),(a,r)=>null}});export{_ as default};
