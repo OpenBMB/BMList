53043c53f054db41c3e4b1acf488494574d00e41
lovangry <272613008@qq.com>
2022-07-29 17:53:29 +0800

    fix: 请求地址

--- bmlist/useData-2ac80d2a.mjs
+++ bmlist/useData-2ac80d2a.mjs
@@ -1,2 +1,2 @@
-/*! build package in 2022/7/29 17:40:10 ! */
+/*! build package in 2022/7/29 17:51:35 ! */
 var e=()=>fetch("https://raw.githubusercontent.com/OpenBMB/BMList/main/all_models.json").then(t=>t.json());export{e as u};
