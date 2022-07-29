793ce9002a49cca644fd1a6a5d7aee8651cb9e55
lovangry <272613008@qq.com>
2022-07-29 17:33:12 +0800

    feat: 更新请求token

--- bmlist/useData-2ac80d2a.mjs
+++ bmlist/useData-2ac80d2a.mjs
@@ -1,2 +1,2 @@
-/*! build package in 2022/7/29 17:27:44 ! */
+/*! build package in 2022/7/29 17:31:32 ! */
 var e=()=>fetch("https://raw.githubusercontent.com/OpenBMB/BMList/main/all_models.json").then(t=>t.json());export{e as u};
