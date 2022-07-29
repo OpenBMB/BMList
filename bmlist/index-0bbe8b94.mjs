32087be762f68c78a5e46e05d8bc2458a2eab8bd
lovangry <272613008@qq.com>
2022-07-29 17:42:09 +0800

    fix: 修改路径

--- bmlist/index-0bbe8b94.mjs
+++ bmlist/index-0bbe8b94.mjs
@@ -1,2 +1,2 @@
-/*! build package in 2022/7/29 17:34:35 ! */
+/*! build package in 2022/7/29 17:40:10 ! */
 import{a as e,b as n}from"./entry-391ec57d.mjs";const _=e({__name:"index",setup(t){return n("/trend"),(a,r)=>null}});export{_ as default};
--- bmlist/useData-2ac80d2a.mjs
+++ bmlist/useData-2ac80d2a.mjs
@@ -1,2 +1,2 @@
-/*! build package in 2022/7/29 17:34:35 ! */
+/*! build package in 2022/7/29 17:40:10 ! */
 var e=()=>fetch("https://raw.githubusercontent.com/OpenBMB/BMList/main/all_models.json").then(t=>t.json());export{e as u};
