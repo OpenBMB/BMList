8a41922aef3a29c2d5fb7031326e37a3e1e474b6
lovangry <272613008@qq.com>
2022-07-29 17:30:14 +0800

    feat: 更新请求star

---
+++ bmlist/index-45e8a9a8.mjs
@@ -0,0 +1,2 @@
+/*! build package in 2022/7/29 17:27:44 ! */
+import{a as e,b as n}from"./entry-b43f0aea.mjs";const _=e({__name:"index",setup(t){return n("/trend"),(a,r)=>null}});export{_ as default};
--- bmlist/useData-2ac80d2a.mjs
+++ bmlist/useData-2ac80d2a.mjs
@@ -1,2 +1,2 @@
-/*! build package in 2022/7/29 17:06:09 ! */
+/*! build package in 2022/7/29 17:27:44 ! */
 var e=()=>fetch("https://raw.githubusercontent.com/OpenBMB/BMList/main/all_models.json").then(t=>t.json());export{e as u};
