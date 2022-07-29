4c0457e9c21cfc19848a9f14f4ea3436d536b535
lovangry <272613008@qq.com>
2022-07-29 17:36:19 +0800

    fix:

--- bmlist/index-e954a6aa.mjs
+++
@@ -1,2 +0,0 @@
-/*! build package in 2022/7/29 17:31:32 ! */
-import{a as e,b as n}from"./entry-18fbc0a2.mjs";const _=e({__name:"index",setup(t){return n("/trend"),(a,r)=>null}});export{_ as default};
--- bmlist/useData-2ac80d2a.mjs
+++ bmlist/useData-2ac80d2a.mjs
@@ -1,2 +1,2 @@
-/*! build package in 2022/7/29 17:31:32 ! */
+/*! build package in 2022/7/29 17:34:35 ! */
 var e=()=>fetch("https://raw.githubusercontent.com/OpenBMB/BMList/main/all_models.json").then(t=>t.json());export{e as u};
