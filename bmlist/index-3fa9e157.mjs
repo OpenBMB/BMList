a7d080e325db45252851c7b3c2cd070216e051fc
lovangry <272613008@qq.com>
2022-07-29 17:11:05 +0800

    feat: 增加统计内容

---
+++ bmlist/index-3fa9e157.mjs
@@ -0,0 +1,2 @@
+/*! build package in 2022/7/29 17:06:09 ! */
+import{a as e,b as n}from"./entry-3adb3e70.mjs";const _=e({__name:"index",setup(t){return n("/trend"),(a,r)=>null}});export{_ as default};
--- bmlist/useData-2ac80d2a.mjs
+++ bmlist/useData-2ac80d2a.mjs
@@ -1,2 +1,2 @@
-/*! build package in 2022/7/28 18:36:01 ! */
+/*! build package in 2022/7/29 17:06:09 ! */
 var e=()=>fetch("https://raw.githubusercontent.com/OpenBMB/BMList/main/all_models.json").then(t=>t.json());export{e as u};
