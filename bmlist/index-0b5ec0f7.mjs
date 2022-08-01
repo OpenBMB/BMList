a9b62ec223264e1dfef3da2c60cfcefb3d637e5e
lovangry <272613008@qq.com>
2022-08-01 11:10:20 +0800

    fix: 获取star

--- bmlist/index-0b5ec0f7.mjs
+++
@@ -1,2 +0,0 @@
-/*! build package in 2022/8/1 10:42:12 ! */
-import{a as e,b as n}from"./entry-91cdad55.mjs";const s=e({__name:"index",setup(t){return n("/list"),(a,o)=>null}});export{s as default};
--- bmlist/useData-2ac80d2a.mjs
+++ bmlist/useData-2ac80d2a.mjs
@@ -1,2 +1,2 @@
-/*! build package in 2022/8/1 10:42:12 ! */
+/*! build package in 2022/8/1 11:09:02 ! */
 var e=()=>fetch("https://raw.githubusercontent.com/OpenBMB/BMList/main/all_models.json").then(t=>t.json());export{e as u};
