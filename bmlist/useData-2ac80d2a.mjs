e694c194edbd9135445286d13121aa35175e085a
lovangry <272613008@qq.com>
2022-08-01 17:38:29 +0800

    feat: 更新list

--- bmlist/useData-2ac80d2a.mjs
+++ bmlist/useData-2ac80d2a.mjs
@@ -1,2 +1,2 @@
-/*! build package in 2022/8/1 13:35:49 ! */
+/*! build package in 2022/8/1 17:34:05 ! */
 var e=()=>fetch("https://raw.githubusercontent.com/OpenBMB/BMList/main/all_models.json").then(t=>t.json());export{e as u};
