77f75189ef3b42957c808f8adf06e044ef91207b
陈林博 <chenlinbo@modelbest.cn>
2022-08-04 11:14:10 +0800

    feat: 自适应

---
+++ bmlist/useState-921ad52e.mjs
@@ -0,0 +1,2 @@
+/*! build package in 2022/8/4 11:07:53 ! */
+import{q as o}from"./entry-2c5c1b23.mjs";const t=()=>o("isMobile",()=>{const{userAgent:e}=globalThis.navigator;var s=["Android","iPhone","SymbianOS","Windows Phone","iPod"];return s.some(a=>e.includes(a))},"$ywSg5JanXc"),n=()=>o("loadStatus",()=>({loading:!0,isLoadFailed:!1}),"$bvUl0fueK5");export{t as a,n as u};
