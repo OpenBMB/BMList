a55b932069c15c4c84bdeabcee4902c01587c087
linbo.chen <chenlinbo@modelbest.cn>
2022-10-12 11:51:58 +0800

    feat: 更新图标

---
+++ bmlist/useState-060be4ea.mjs
@@ -0,0 +1,2 @@
+/*! build package in 2022/10/12 11:48:55 ! */
+import{q as o}from"./entry-8823e517.mjs";const t=()=>o("isMobile",()=>{const{userAgent:e}=globalThis.navigator;var s=["Android","iPhone","SymbianOS","Windows Phone","iPod"];return s.some(a=>e.includes(a))},"$ywSg5JanXc"),n=()=>o("loadStatus",()=>({loading:!0,isLoadFailed:!1}),"$bvUl0fueK5");export{t as a,n as u};
--- bmlist/useState-9157f611.mjs
+++
@@ -1,2 +0,0 @@
-/*! build package in 2022/9/20 10:21:18 ! */
-import{q as o}from"./entry-ab3e2b8f.mjs";const t=()=>o("isMobile",()=>{const{userAgent:e}=globalThis.navigator;var s=["Android","iPhone","SymbianOS","Windows Phone","iPod"];return s.some(a=>e.includes(a))},"$ywSg5JanXc"),n=()=>o("loadStatus",()=>({loading:!0,isLoadFailed:!1}),"$bvUl0fueK5");export{t as a,n as u};
